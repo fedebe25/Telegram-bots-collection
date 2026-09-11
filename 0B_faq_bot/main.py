1b7b4078a28742c11a72c3a37ba2b26306ddaa1c
fedebe25 <fede.betti08@gmail.com>
2026-09-11 21:52:03 +0200

    feat:gestione click bottoni, filtro messaggi di testo e pulizia log

--- 0B_faq_bot/main.py
+++ 0B_faq_bot/main.py
@@ -3,7 +3,7 @@ import logging
 from venv import logger
 from dotenv import load_dotenv
 from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -11,6 +11,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+logging.logger("httpx").setLevel(logging.WARNING)
 
 load_dotenv()
 
@@ -18,7 +19,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update:Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        ["Domande frequenti", "Contatta un umano"]
+        ["Domande frequenti", "Contatta un umano"],
         ["Orari e Info"]
     ]
 
@@ -31,9 +32,29 @@ async def start(update:Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
+async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    testo_utente = update.message.text
+
+    logger.info(f"Ricevuto messaggio: '{testo_utente}' da {update.message.from_user.first_name}")
+    
+    if testo_utente == "Domande Frequenti ❓":
+        await update.message.reply_text("Hai scelto le FAQ! Presto collegheremo questa sezione al database.")
+        
+    elif testo_utente == "Contatta un Umano 🙋‍♂️":
+        await update.message.reply_text("Richiesta di assistenza registrata. Inoltro al proprietario in corso...")
+        
+    elif testo_utente == "Orari e Info 🕒":
+        await update.message.reply_text("Siamo operativi tutti i giorni, pronti a risponderti.")
+        
+    else:
+        await update.message.reply_text("Scusami, non ho capito. Per favore, usa i bottoni del menu qui sotto.")
+
+
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
     
     logger.info("Bot in esecuzione... Premi CTRL+C per spegnerlo.")
     app.run_polling()
